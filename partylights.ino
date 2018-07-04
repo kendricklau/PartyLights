2359266de611fc4bdfd7c2d41099290cfa966923
Kendrick Lau <kendrick@KendrickMBP.lfytina2.metronetinc.net>
2018-07-04 03:20:56 -0400

    Added Glide and Averaging

--- partylights.ino
+++ partylights.ino
@@ -13,19 +13,27 @@
 int analogPin = 1; // MSGEQ7 OUT
 int strobePin = 2; // MSGEQ7 STROBE
 int resetPin = 3; // MSGEQ7 RESET
+int prevSpectrumValue[7];
+int glideValue[7];
 int spectrumValue[7];
- 
+
+int prevArray1[7];
+int prevArray2[7];
+int prevArray3[7];
 // MSGEQ7 OUT pin produces values around 50-80
 // when there is no input, so use this value to
 // filter out a lot of the chaff.
-int filterValue = 160;
+int filterValue = 70;
  
 // LED pins connected to the PWM pins on the Arduino
 
 int ledPinR = 9;
 int ledPinG = 10;
 int ledPinB = 11;
- 
+
+String space = " ";
+
+
 void setup()
 {
   Serial.begin(9600);
@@ -56,35 +64,53 @@ void loop()
     delayMicroseconds(30); // Allow output to settle
  
     spectrumValue[i] = analogRead(analogPin);
- 
+    //Serial.print(spectrumValue[i]);
+    //Serial.print(" ");
     // Constrain any value above 1023 or below filterValue
     spectrumValue[i] = constrain(spectrumValue[i], filterValue, 1023);
 
     // Remap the value to a number between 0 and 255
     spectrumValue[i] = map(spectrumValue[i], filterValue, 1023, 0, 255);
     //filter out static noise
-    if(spectrumValue[i] < 10){
-      spectrumValue[i] = 0;
+    if(spectrumValue[i] < 25){
+      spectrumValue[i] = 10;
     }
     // Remove serial stuff after debugging
-    Serial.print(spectrumValue[i]);
-    Serial.print(" ");
+    //Serial.print(spectrumValue[i]);
+    //Serial.print(" ");
     digitalWrite(strobePin, HIGH);
    }
- 
-   Serial.println();
- 
-   // Write the PWM values to the LEDs
-   // I find that with three LEDs, these three spectrum values work the best
-   //Serial.print(spectrumValue[1]);
-   //Serial.print(" ");
-   //Serial.print(spectrumValue[4]);
-   //Serial.print(" ");
-   //Serial.print(spectrumValue[6]);
-   //Serial.print(" ");
-   
-   analogWrite(ledPinR, spectrumValue[1]); 
-   analogWrite(ledPinG, spectrumValue[4]); 
-   analogWrite(ledPinB, spectrumValue[6]); 
 
+   averageArray(spectrumValue);
+   writeGlide(spectrumValue);
 }
+
+void averageArray(int (& myArray) [7])
+{
+  memcpy(prevArray1, prevArray2, sizeof(prevArray1));
+  memcpy(prevArray2, prevArray3, sizeof(prevArray2));
+  memcpy(prevArray3, myArray, sizeof(prevArray3));
+  for (int i = 0; i < 7; i++)
+  {
+    myArray[i] = (prevArray1[i] + prevArray2[i] + prevArray3[i])/3;
+  }
+}
+
+void writeGlide(int (& myArray) [7])
+{
+  memcpy(prevSpectrumValue, myArray, sizeof(prevSpectrumValue));
+  
+  for (int j = 0; j < 100; j++)
+  {
+    for (int i = 0; i < 7; i++)
+    {
+      glideValue[i] = (myArray[i] - prevSpectrumValue[i])/100;
+      myArray[i] = myArray[i] + glideValue[i];
+    }
+    analogWrite(ledPinR, (myArray[1] + myArray[2])/2); 
+    analogWrite(ledPinG, (myArray[4] + myArray[3])/2); 
+    analogWrite(ledPinB, (myArray[6] + myArray[5])/2); 
+  }
+}
+
+
